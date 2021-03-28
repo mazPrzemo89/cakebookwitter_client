9f8f8c875ab7e268db54b582d7aff415cb695cc1
przem <maz89prz@gmail.com>
2021-03-28 16:56:42 +0100

    added a home button

--- src/layoutComnonents/Header.tsx
+++ src/layoutComnonents/Header.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import '../styles/header.css'
+import homeButton from '../icons/house.png'
 
 const Header: React.FC = () => {
+  const location = useLocation()
   return (
     <div className="headerDiv">
       <Link to="/" className="headerLink">
         <h1 className="headerText">Cakebookwitter</h1>
+        {location.pathname !== '/' && <img src={homeButton} />}
       </Link>
     </div>
   )
